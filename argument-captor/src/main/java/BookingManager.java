fc9cbc4f05e8f564298a9c14705aa4dc5b791375
Siegfried <bachbrahms@163.com>
2023-07-29 12:11:32 +0800

    addition

--- argument-captor/src/main/java/BookingManager.java
+++ argument-captor/src/main/java/BookingManager.java
@@ -8,6 +8,6 @@ public class BookingManager {
     public void addReservation(String name, Integer number, Integer time) {
         Table table = new Table(name, number, time);
         Booking booking = new Booking(table);
-        bookingPersister.persistToDb(booking); // this method call with specific argument needs to be test
+        bookingPersister.persistToDb(booking); //this method call with specific argument needs to be test
     }
 }
